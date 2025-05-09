2d4ffe2af4225331e2079a2d8f48cbe16bd42fb1
Eric Boedo <ericboedo01@gmail.com>
2025-05-09 20:58:01 -0300

    Update and rename Agenda.java to Disponibilidad.java

--- src/datos/Disponibilidad.java
+++ src/datos/Disponibilidad.java
@@ -3,21 +3,31 @@ package datos;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Agenda {
+public class Disponibilidad {
 	private long id;
 	private LocalDate date;
 	private LocalTime time;
 	private boolean blocked;
+	private Profesional profesional;
 	
-	public Agenda(LocalDate date, LocalTime time, boolean blocked) {
+	public Disponibilidad(LocalDate date, LocalTime time, boolean blocked, Profesional profesional) {
 		super();
 		this.date = date;
 		this.time = time;
 		this.blocked = blocked;
+		this.profesional = profesional;
 	}
 
+	public Disponibilidad() {
+		
+	}
+	
 	public long getId() {
 		return id;
+	}
+
+	protected void setId(long id) {
+		this.id = id;
 	}
 
 	public LocalDate getDate() {
@@ -43,7 +53,19 @@ public class Agenda {
 	public void setBlocked(boolean blocked) {
 		this.blocked = blocked;
 	}
-	
-	
 
+	public Profesional getProfesional() {
+		return profesional;
+	}
+
+	public void setProfesional(Profesional profesional) {
+		this.profesional = profesional;
+	}
+
+	@Override
+	public String toString() {
+		return "Disponibilidad [id=" + id + ", Fecha=" + date + ", Hora=" + time + ", Ocupado=" + blocked + "]";
+	}
+	
 }
+
