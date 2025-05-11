8a6373e638c12645dd57c33407ae7e8eb7c06b89
razoruwu <irreranicolas8@gmail.com>
2025-05-11 20:40:45 -0300

    casos de uso en cliente, turno

--- src/test/TestServicio.java
+++ src/test/TestServicio.java
@@ -7,8 +7,8 @@ import negocio.ServicioABM;
 public class TestServicio {
     public static void main(String[] args) {
        ServicioABM abm = new ServicioABM();
-       Servicio servicio= new Servicio("hola", "si", "20 a;os ", 12237489.0);
-        Servicio servicio2= new Servicio("juan", "lavado de patas", "2 minutos", 2.0);
+       Servicio servicio= new Servicio("hola", "si", "20 años ", 12237489.0);
+        Servicio servicio2= new Servicio("juan", "lavado de auto", "2 minutos", 2.0);
        try {
            abm.agregar(servicio2);
 
